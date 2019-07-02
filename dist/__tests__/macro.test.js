c87186b7deba126bce53c0b50754ef72164009bf
Solomon Hawk <solomon.hawk@viget.com>
2019-07-02 12:22:32 -0400

    Remove extra root folder

--- dist/__tests__/macro.test.js
+++ dist/__tests__/macro.test.js
@@ -4,5 +4,26 @@ const plugin = require('babel-plugin-macros')
 
 pluginTester({
   plugin,
-  fixtures: path.join(__dirname, '..', '__fixtures__', 'positive')
+  fixtures: path.join(__dirname, '..', '__fixtures__'),
+  tests: [
+    {
+      code: `
+        import macro from './json-parse.macro'
+
+        const value = {
+          string: 'string',
+          number: 1,
+          array: [1, 2, 3],
+          object: {
+            foo: 'bar'
+          }
+        }
+
+        console.log(value)
+
+        const obj = macro(value)
+      `,
+      error: /json-parse.macro cannot optimize bindings that are referenced by other code\. Ensure the variable passed to `macro\(..\)` is not referenced elsewhere\./
+    }
+  ]
 })
